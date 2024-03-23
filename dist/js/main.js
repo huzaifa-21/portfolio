de952c142b3964b7b242a13a061c24372fd1c222
7ozaifa <huzaifasalah9@gmail.com>
2024-03-24 03:03:20 +0400

    finish the validation for the form

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,10 +12,7 @@ window.onscroll = () => {
     handelActiveScroll("home");
   }
 
-  if (
-    Math.trunc(window.scrollY) > 500 &&
-    getBottom(skillSection,500) 
-  ) {
+  if (Math.trunc(window.scrollY) > 500 && getBottom(skillSection, 500)) {
     skillBox.forEach((skill) => {
       skill.style = "animation-play-state:running;";
     });
@@ -23,12 +20,11 @@ window.onscroll = () => {
   }
   if (
     Math.trunc(window.scrollY) > Math.trunc(projectSection.offsetTop - 100) &&
-    getBottom(projectSection,200)
+    getBottom(projectSection, 200)
   ) {
     handelActiveScroll("projects");
   }
 };
-
 
 function handelActive() {
   links.forEach((link) => {
@@ -63,20 +59,19 @@ function handleVideoPlay() {
 
 handleVideoPlay();
 
-function getBottom(section,number) {
+function getBottom(section, number) {
   let offsetBottom = section.offsetTop + section.offsetHeight;
-  let bottom= Math.trunc(window.scrollY) < Math.trunc(offsetBottom - number);
+  let bottom = Math.trunc(window.scrollY) < Math.trunc(offsetBottom - number);
   return bottom;
 }
 // let projectBottom = projectSection.offsetTop + projectSection.offsetHeight;
-
-
 
 //====== start handling email sending from the client side =============
 let form = document.querySelector("form");
 let clientName = document.getElementById("from_name");
 let clientMail = document.getElementById("email_id");
 let clientMessage = document.getElementById("message");
+let inputs = document.querySelectorAll("input ,textarea");
 
 form.onsubmit = (e) => {
   e.preventDefault();
@@ -85,13 +80,54 @@ form.onsubmit = (e) => {
     email_id: clientMail.value,
     message: clientMessage.value,
   };
-  emailjs.send("service_9jmy653", "template_6l5xirh", params).then(
-    (response) => {
-      console.log("SUCCESS!", response.status, response.text);
-    },
-    (error) => {
-      console.log("FAILED...", error);
-    }
-  );
+  let nameValid = false;
+  let mailValid = false;
+  let messageValid = false;
+
+  let { from_name: name, email_id: mail, message: msg } = params;
+  //================== start name validation ================================
+
+  if (name.length > 0 && isNaN(name)) {
+    nameValid = true;
+  } else {
+    clientName.parentElement.classList.add("not-valid");
+  }
+  //================== end name validation ==================================
+
+  //================== start email validation ===============================
+  // regular experssion for email validation
+  let mailReg = /(\D+?|\D+\w+)@\w+.\w+/g;
+  if (mail.length > 0 && mail.match(mailReg)) {
+    mailValid = true;
+  } else {
+    clientMail.parentElement.classList.add("not-valid");
+  }
+  //================== end email validation ===============================
+  //================== start message validation ===========================
+  if (msg.length > 0) {
+    messageValid = true;
+  } else {
+    clientMessage.parentElement.classList.add("not-valid");
+  }
+  //================== end message validation =============================
+  if (nameValid && mailValid && messageValid) {
+    emailjs.send("service_9jmy653", "template_6l5xirh", params).then(
+      (response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        clientMail.value = "";
+        clientName.value = "";
+        clientMessage.value = "";
+      },
+      (error) => {
+        console.log("FAILED...", error);
+      }
+    );
+  }
 };
+
+[...inputs].forEach((input) => {
+  input.oninput = (e) => {
+    e.target.parentElement.classList.remove("not-valid");
+  };
+});
 //====== end handling email sending from the client side =============
